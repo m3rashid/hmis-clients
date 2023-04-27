6d5e4d64963ee3ca278632f339c6ec218f4a8f3b
m3rashid <m3rashid.hussain@gmail.com>
2023-04-27 18:53:20 +0530

    feat(init): react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,30 @@
 import App from 'App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { UiContextProvider } from 'context/ui'
 import { BrowserRouter } from 'react-router-dom'
 import AppLayout from 'components/globals/layout'
-import { ConfigContextProvider } from 'context/config'
-import { UiContextProvider } from 'context/ui'
 import { AuthContextProvider } from 'context/auth'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ConfigContextProvider } from 'context/config'
 
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<ConfigContextProvider>
-				<UiContextProvider>
-					<AuthContextProvider>
-						<AppLayout>
-							<App />
-						</AppLayout>
-					</AuthContextProvider>
-				</UiContextProvider>
-			</ConfigContextProvider>
+			<QueryClientProvider client={queryClient}>
+				<ConfigContextProvider>
+					<UiContextProvider>
+						<AuthContextProvider>
+							<AppLayout>
+								<App />
+							</AppLayout>
+						</AuthContextProvider>
+					</UiContextProvider>
+				</ConfigContextProvider>
+			</QueryClientProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 )
